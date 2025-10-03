3bfe0a71fe5cfe775226e58ebac0ee482049428a
Isaac <57078036+MrMiffs@users.noreply.github.com>
2025-10-02 19:44:20 -0600

    Homework 2 - Created models

--- homework2/movie_theatre_booking/booking/models.py
+++ homework2/movie_theatre_booking/booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    release_date = models.DateField()
+    duration = models.DurationField()
+
+
+class Seat(models.Model):
+    seat_number = models.IntegerField()
+    booking_status = models.BooleanField(default=False)
+
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    seat = models.ForeignKey(Seat, on_delete=models.CASCADE)
+    booking_date = models.DateField()
