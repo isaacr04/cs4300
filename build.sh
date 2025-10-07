bd6d9c71aa60d9711a5337b4010cd0ae77761b9a
Isaac <57078036+MrMiffs@users.noreply.github.com>
2025-10-06 22:01:13 -0600

    Homework 2 - Fifth Render Attempt
    Moved build.sh and added 'cd' commands to yaml file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-pip install -r homework2/movie_theatre_booking/requirements.txt
-
-python homework2/movie_theatre_booking/manage.py collectstatic --no-input
-
-python homework2/movie_theatre_booking/manage.py migrate
